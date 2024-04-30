9475280df60bd1d7e332484e52f9b4accb1c67e2
Xander Gallegos <aranxa.gallegos@iteso.mx>
2024-04-30 15:54:51 -0600

    gradio app

---
+++ app.py
@@ -0,0 +1,16 @@
+from chatbot import ChatModel
+import gradio as gr
+
+chat_model = ChatModel('shrek-script-pdf.pdf')
+
+def chat_bot(input_question, history):
+    response = chat_model.answer(input_question)
+    return response['answer']
+
+
+iface = gr.ChatInterface(fn=chat_bot,
+                         title="Shrek-Bot",
+                         description="Hey there! I'm Shrek-Bot, here to guide you through the \
+                         wondrous world of one of the greatest movies ever made: Shrek 1! Go ahead, \
+                         put me to the test - ask me anything!")
+iface.launch()
